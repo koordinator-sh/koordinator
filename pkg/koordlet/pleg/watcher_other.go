85859b3bf153d73561b045d6ecb7143f81dd60c7
Jason Liu <jasonliu747@gmail.com>
2022-03-25 11:48:48 +0800

    add copyright
    Signed-off-by: Jason Liu <jasonliu747@gmail.com>

--- pkg/koordlet/pleg/watcher_other.go
+++ pkg/koordlet/pleg/watcher_other.go
@@ -2,7 +2,7 @@
 // +build !linux
 
 /*
-Copyright 2021 Alibaba Cloud.
+Copyright 2022 The Koordinator Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
