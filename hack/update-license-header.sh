4b881323913c98815631eca58737040eccf93ed8
Jason Liu <jasonliu747@gmail.com>
2022-05-27 16:00:35 +0800

    vendor: goodbye vendor
    Signed-off-by: Jason Liu <jasonliu747@gmail.com>

--- hack/update-license-header.sh
+++ hack/update-license-header.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+#
+# Copyright 2022 The Koordinator Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 PROJECT=$(cd $(dirname $0)/..; pwd)
 
@@ -6,4 +21,4 @@ LICENSEHEADERCHECKER_VERSION=v1.3.0
 
 GOBIN=${PROJECT}/bin go install github.com/lsm-dev/license-header-checker/cmd/license-header-checker@${LICENSEHEADERCHECKER_VERSION}
 
-${PROJECT}/bin/license-header-checker -r -a -v -i vendor ${PROJECT}/hack/boilerplate.go.txt . go
+${PROJECT}/bin/license-header-checker -r -a -v -i vendor ${PROJECT}/hack/boilerplate/boilerplate.go.txt . go
