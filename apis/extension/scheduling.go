bb3065a53ee881c57b2c7e4c329d1d7e1eadccfb
Vans7 <wen470366661@163.com>
2022-08-02 21:02:46 +0800

    apis: add Gang api definition (#409)
    Signed-off-by: wenshiqi <wen470366661@163.com>

--- apis/extension/scheduling.go
+++ apis/extension/scheduling.go
@@ -33,6 +33,36 @@ const (
 	// AnnotationReservationAllocated represents the reservation allocated by the pod.
 	AnnotationReservationAllocated = SchedulingDomainPrefix + "/reservation-allocated"
 )
+const (
+	AnnotationGangPrefix = "gang.scheduling.koordinator.sh"
+	// AnnotationGangName specifies the name of the gang
+	AnnotationGangName = AnnotationGangPrefix + "/name"
+
+	// AnnotationGangMinNum specifies the minimum number of the gang that can be executed
+	AnnotationGangMinNum = AnnotationGangPrefix + "/min-available"
+
+	// AnnotationGangWaitTime specifies gang's max wait time in Permit Stage
+	AnnotationGangWaitTime = AnnotationGangPrefix + "/waiting-time"
+
+	// AnnotationGangTotalNum specifies the total children number of the gang
+	// If not specified,it will be set with the AnnotationGangMinNum
+	AnnotationGangTotalNum = AnnotationGangPrefix + "/total-number"
+
+	// AnnotationGangMode defines the Gang Scheduling operation when failed scheduling
+	// Support GangModeStrict and GangModeNonStrict, default is GangModeStrict
+	AnnotationGangMode = AnnotationGangPrefix + "/mode"
+
+	// AnnotationGangGroups defines which gangs are bundled as a group
+	// The gang will go to bind only all gangs in one group meet the conditions
+	AnnotationGangGroups = AnnotationGangPrefix + "/groups"
+
+	// AnnotationGangTimeout means that the entire gang cannot be scheduled due to timeout
+	// The annotation is added by the scheduler when the gang times out
+	AnnotationGangTimeout = AnnotationGangPrefix + "/timeout"
+
+	GangModeStrict    = "Strict"
+	GangModeNonStrict = "NonStrict"
+)
 
 // CustomUsageThresholds supports user-defined node resource utilization thresholds.
 type CustomUsageThresholds struct {
