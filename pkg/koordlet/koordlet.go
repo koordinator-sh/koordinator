5bafc854e0f7c5a1a969631343ff00ef5ba781be
stormgbs <stormgbs@gmail.com>
2022-08-31 12:50:35 +0800

    QoS Manager
    Signed-off-by: stormgbs <stormgbs@gmail.com>

--- pkg/koordlet/koordlet.go
+++ pkg/koordlet/koordlet.go
@@ -36,6 +36,7 @@ import (
 	"github.com/koordinator-sh/koordinator/pkg/koordlet/metrics"
 	"github.com/koordinator-sh/koordinator/pkg/koordlet/metricsadvisor"
 	"github.com/koordinator-sh/koordinator/pkg/koordlet/pleg"
+	"github.com/koordinator-sh/koordinator/pkg/koordlet/qosmanager"
 	"github.com/koordinator-sh/koordinator/pkg/koordlet/reporter"
 	"github.com/koordinator-sh/koordinator/pkg/koordlet/resmanager"
 	"github.com/koordinator-sh/koordinator/pkg/koordlet/runtimehooks"
@@ -61,6 +62,7 @@ type daemon struct {
 	metricCache    metriccache.MetricCache
 	reporter       reporter.Reporter
 	resManager     resmanager.ResManager
+	qosManager     qosmanager.QoSManager
 	runtimeHook    runtimehooks.RuntimeHook
 	pleg           pleg.Pleg
 }
@@ -121,6 +123,8 @@ func NewDaemon(config *config.Configuration) (Daemon, error) {
 
 	resManagerService := resmanager.NewResManager(config.ResManagerConf, scheme, kubeClient, crdClient, nodeName, statesInformer, metricCache, int64(config.CollectorConf.CollectResUsedIntervalSeconds))
 
+	qosManager := qosmanager.NewQosManager(config.QosManagerConf, scheme, kubeClient, nodeName, statesInformer, metricCache)
+
 	runtimeHook, err := runtimehooks.NewRuntimeHook(statesInformer, config.RuntimeHookConf)
 	if err != nil {
 		return nil, err
@@ -132,6 +136,7 @@ func NewDaemon(config *config.Configuration) (Daemon, error) {
 		metricCache:    metricCache,
 		reporter:       reporterService,
 		resManager:     resManagerService,
+		qosManager:     qosManager,
 		runtimeHook:    runtimeHook,
 		pleg:           pleg,
 	}
@@ -184,6 +189,14 @@ func (d *daemon) Run(stopCh <-chan struct{}) {
 	go func() {
 		if err := d.resManager.Run(stopCh); err != nil {
 			klog.Error("Unable to run the resManager: ", err)
+			os.Exit(1)
+		}
+	}()
+
+	// start QoS Manager
+	go func() {
+		if err := d.qosManager.Run(stopCh); err != nil {
+			klog.Error("Unable to run the QoSManager: ", err)
 			os.Exit(1)
 		}
 	}()
---
+++ pkg/koordlet/qosmanager/config/config.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2022 The Koordinator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+import (
+	"flag"
+	"strings"
+
+	"k8s.io/apimachinery/pkg/util/runtime"
+	cliflag "k8s.io/component-base/cli/flag"
+	"k8s.io/component-base/featuregate"
+
+	"github.com/koordinator-sh/koordinator/pkg/koordlet/qosmanager/plugins"
+	"github.com/koordinator-sh/koordinator/pkg/koordlet/qosmanager/plugins/cpuburst"
+	"github.com/koordinator-sh/koordinator/pkg/koordlet/qosmanager/plugins/cpuevict"
+	"github.com/koordinator-sh/koordinator/pkg/koordlet/qosmanager/plugins/cpusuppress"
+)
+
+var (
+	DefaultMutableQoSManagerFG featuregate.MutableFeatureGate = featuregate.NewFeatureGate()
+	DefaultQoSManagerFG        featuregate.FeatureGate        = DefaultMutableQoSManagerFG
+
+	cpuSuppressFeature = new(cpusuppress.CPUSuppress).Feature()
+	cpuBurstFeature    = new(cpuburst.CPUBurst).Feature()
+	cpuEvictFeature    = new(cpuevict.CPUEvictor).Feature()
+
+	defaultQoSManagerFG = map[featuregate.Feature]featuregate.FeatureSpec{
+		cpuSuppressFeature: {Default: false, PreRelease: featuregate.Alpha},
+		cpuBurstFeature:    {Default: false, PreRelease: featuregate.Alpha},
+		cpuEvictFeature:    {Default: false, PreRelease: featuregate.Alpha},
+	}
+
+	QoSPluginFactories = map[featuregate.Feature]plugins.PluginFactoryFn{
+		cpuSuppressFeature: cpusuppress.New,
+		cpuBurstFeature:    cpuburst.New,
+		cpuEvictFeature:    cpuevict.New,
+	}
+)
+
+type Config struct {
+	FeatureGates       map[string]bool
+	PluginExtraConfigs map[string]string
+}
+
+func NewDefaultConfig() *Config {
+	return &Config{
+		FeatureGates:       map[string]bool{},
+		PluginExtraConfigs: map[string]string{},
+	}
+}
+
+func (c *Config) InitFlags(fs *flag.FlagSet) {
+	fs.Var(cliflag.NewMapStringBool(&c.FeatureGates), "qos-plugins",
+		"A set of key=value pairs that describe feature gates for QoS Manager plugins alpha/experimental features. "+
+			"Options are:\n"+strings.Join(DefaultQoSManagerFG.KnownFeatures(), "\n"))
+}
+
+func init() {
+	runtime.Must(DefaultMutableQoSManagerFG.Add(defaultQoSManagerFG))
+}
