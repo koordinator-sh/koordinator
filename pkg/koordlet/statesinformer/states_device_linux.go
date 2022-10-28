b6e9cda6e9fafd3b13151dc622f20fed0e8bbe1b
buptcozy <70878006+buptcozy@users.noreply.github.com>
2022-10-28 11:19:14 +0800

    koordlet report gpu model and driver version (#745)
    Signed-off-by: yangzhang <bupt_cozy@126.com>

--- pkg/koordlet/statesinformer/states_device_linux.go
+++ pkg/koordlet/statesinformer/states_device_linux.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"os"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/NVIDIA/go-nvml/pkg/nvml"
@@ -49,8 +50,16 @@ func generateQueryParam() *metriccache.QueryParam {
 func (s *statesInformer) reportDevice() {
 	node := s.GetNode()
 	gpuDevices := s.buildGPUDevice()
+	if len(gpuDevices) == 0 {
+		return
+	}
 
-	err := s.updateDevice(node.Name, gpuDevices)
+	gpuModel, gpuDriverVer := s.getGPUDriverAndModelFunc()
+
+	device := s.buildBasicDevice(node)
+	s.fillGPUDevice(device, gpuDevices, gpuModel, gpuDriverVer)
+
+	err := s.updateDevice(device)
 	if err == nil {
 		klog.V(4).Infof("successfully update Device %s", node.Name)
 		return
@@ -60,11 +69,7 @@ func (s *statesInformer) reportDevice() {
 		return
 	}
 
-	if len(gpuDevices) == 0 {
-		return
-	}
-
-	err = s.createDevice(node, gpuDevices)
+	err = s.createDevice(device)
 	if err == nil {
 		klog.V(4).Infof("successfully create Device %s", node.Name)
 	} else {
@@ -72,7 +77,7 @@ func (s *statesInformer) reportDevice() {
 	}
 }
 
-func (s *statesInformer) createDevice(node *corev1.Node, gpuDevices []schedulingv1alpha1.DeviceInfo) error {
+func (s *statesInformer) buildBasicDevice(node *corev1.Node) *schedulingv1alpha1.Device {
 	blocker := true
 	device := &schedulingv1alpha1.Device{
 		ObjectMeta: metav1.ObjectMeta{
@@ -88,38 +93,53 @@ func (s *statesInformer) createDevice(node *corev1.Node, gpuDevices []scheduling
 				},
 			},
 		},
-		Spec: schedulingv1alpha1.DeviceSpec{
-			Devices: gpuDevices,
-		},
 	}
+
+	return device
+}
+
+func (s *statesInformer) fillGPUDevice(device *schedulingv1alpha1.Device,
+	gpuDevices []schedulingv1alpha1.DeviceInfo, gpuModel string, gpuDriverVer string) {
+
+	device.Spec.Devices = append(device.Spec.Devices, gpuDevices...)
+	if device.Labels == nil {
+		device.Labels = make(map[string]string)
+	}
+	if gpuModel != "" {
+		device.Labels[extension.GPUModel] = gpuModel
+	}
+	if gpuDriverVer != "" {
+		device.Labels[extension.GPUDriver] = gpuDriverVer
+	}
+}
+
+func (s *statesInformer) createDevice(device *schedulingv1alpha1.Device) error {
 	_, err := s.deviceClient.Create(context.TODO(), device, metav1.CreateOptions{})
 	return err
 }
 
-func (s *statesInformer) updateDevice(name string, gpuDevices []schedulingv1alpha1.DeviceInfo) error {
+func (s *statesInformer) updateDevice(deviceNew *schedulingv1alpha1.Device) error {
 	sorter := func(devices []schedulingv1alpha1.DeviceInfo) {
 		sort.Slice(devices, func(i, j int) bool {
 			return *(devices[i].Minor) < *(devices[j].Minor)
 		})
 	}
-	sorter(gpuDevices)
+	sorter(deviceNew.Spec.Devices)
 
 	return util.RetryOnConflictOrTooManyRequests(func() error {
-		device, err := s.deviceClient.Get(context.TODO(), name, metav1.GetOptions{ResourceVersion: "0"})
+		deviceOld, err := s.deviceClient.Get(context.TODO(), deviceNew.Name, metav1.GetOptions{ResourceVersion: "0"})
 		if err != nil {
 			return err
 		}
-		sorter(device.Spec.Devices)
+		sorter(deviceOld.Spec.Devices)
 
-		if apiequality.Semantic.DeepEqual(gpuDevices, device.Spec.Devices) {
-			klog.V(4).Infof("Device %s has not changed and does not need to be updated", name)
+		if apiequality.Semantic.DeepEqual(deviceNew.Spec.Devices, deviceOld.Spec.Devices) &&
+			apiequality.Semantic.DeepEqual(deviceNew.Labels, deviceOld.Labels) {
+			klog.V(4).Infof("Device %s has not changed and does not need to be updated", deviceNew.Name)
 			return nil
 		}
 
-		device.Spec = schedulingv1alpha1.DeviceSpec{
-			Devices: gpuDevices,
-		}
-		_, err = s.deviceClient.Update(context.TODO(), device, metav1.UpdateOptions{})
+		_, err = s.deviceClient.Update(context.TODO(), deviceNew, metav1.UpdateOptions{})
 		return err
 	})
 }
@@ -169,6 +189,64 @@ func (s *statesInformer) initGPU() bool {
 		return false
 	}
 	return true
+}
+
+func (s *statesInformer) getGPUDriverAndModel() (string, string) {
+	count, ret := nvml.DeviceGetCount()
+	if ret != nvml.SUCCESS {
+		klog.Errorf("unable to get device count: %v", nvml.ErrorString(ret))
+		return "", ""
+	}
+
+	if count == 0 {
+		klog.Errorf("no gpu device found")
+		return "", ""
+	}
+
+	var modelList []string
+	for deviceIndex := 0; deviceIndex < count; deviceIndex++ {
+		gpuDevice, ret := nvml.DeviceGetHandleByIndex(deviceIndex)
+		if ret != nvml.SUCCESS {
+			klog.Errorf("unable to get device model: %v", nvml.ErrorString(ret))
+			continue
+		}
+		deviceModel, _ := gpuDevice.GetName()
+		modelList = append(modelList, deviceModel)
+	}
+
+	model := ""
+	for i, v := range modelList {
+		if v == "" {
+			klog.Errorf("device model invalid: %v", modelList)
+			return "", ""
+		} else if i == 0 {
+			model = v
+		} else if model != v {
+			klog.Errorf("device model invalid: %v", modelList)
+			return "", ""
+		}
+	}
+
+	// NVIDIA Driver 470 report GPU Model with "NVIDIA " Prefix
+	model = strings.TrimPrefix(model, "NVIDIA ")
+
+	// A100 SXM4 80GB -> A100-SXM4-80GB
+	// Tesla P100-PCIE-16GB -> Tesla-P100-PCIE-16GB
+	// Tesla V100-SXM2-16GB -> Tesla-V100-SXM2-16GB
+	// Tesla T4 -> Tesla-T4
+	// Tesla P40 -> Tesla-P40
+	// Tesla M40 -> Tesla-M40
+	// GeForce RTX 2080 Ti -> GeForce-RTX-2080-Ti
+	// GeForce GTX 1080 Ti -> GeForce-GTX-1080-Ti
+	transModel := strings.ReplaceAll(model, " ", "-")
+
+	driverVersion, ret := nvml.SystemGetDriverVersion()
+	if ret != nvml.SUCCESS {
+		klog.Errorf("unable to get device driver version: %v", nvml.ErrorString(ret))
+		return "", ""
+	}
+
+	return transModel, driverVersion
 }
 
 func (s *statesInformer) gpuHealCheck(stopCh <-chan struct{}) {
