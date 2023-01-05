06bb2c578b5460b19ba4e597da8e0b9390d77a9e
LeoLiuYan <929908264@qq.com>
2023-01-05 15:06:16 +0800

    koord-manager: Fix log print fmt and add an example for slo-controller-config (#925)
    Signed-off-by: LeoLiuYan <929908264@qq.com>

--- pkg/slo-controller/config/constants.go
+++ pkg/slo-controller/config/constants.go
@@ -33,5 +33,187 @@ Koordinator uses configmap to manage the configuration of SLO, the configmap is 
    - <CPUBurstConfigKey>
 
 et.
-  TODO add a sample here
+
+For example, the configmap is as follows:
+
+```
+apiVersion: v1
+data:
+  colocation-config: |
+    {
+      "enable": false,
+      "metricAggregateDurationSeconds": 300,
+      "metricReportIntervalSeconds": 60,
+      "metricAggregatePolicy": {
+        "durations": [
+          {
+            "duration": "5m"
+          },
+          {
+            "duration": "10m"
+          },
+          {
+            "duration": "15m"
+          }
+        ]
+      },
+      "cpuReclaimThresholdPercent": 60,
+      "memoryReclaimThresholdPercent": 65,
+      "memoryCalculatePolicy": "usage",
+      "degradeTimeMinutes": 15,
+      "updateTimeThresholdSeconds": 300,
+      "resourceDiffThreshold": 0.1,
+      "nodeConfigs": [
+        {
+          "nodeSelector": {
+            "matchLabels": {
+              "kubernetes.io/kernel": "alios"
+            }
+          },
+          "updateTimeThresholdSeconds": 360,
+          "resourceDiffThreshold": 0.2
+        }
+      ]
+    }
+  cpu-burst-config: |
+    {
+      "clusterStrategy": {
+        "policy": "none",
+        "cpuBurstPercent": 1000,
+        "cfsQuotaBurstPercent": 300,
+        "cfsQuotaBurstPeriodSeconds": -1,
+        "sharePoolThresholdPercent": 50
+      },
+      "nodeStrategies": [
+        {
+          "nodeSelector": {
+            "matchLabels": {
+              "kubernetes.io/kernel": "alios"
+            }
+          },
+          "policy": "cfsQuotaBurstOnly",
+          "cfsQuotaBurstPercent": 400
+        }
+      ]
+    }
+  resource-qos-config: |
+    {
+      "clusterStrategy": {
+        "lsrClass": {
+          "cpuQOS": {
+            "enable": false,
+            "groupIdentity": 2
+          },
+          "memoryQOS": {
+            "enable": false,
+            "minLimitPercent": 0,
+            "lowLimitPercent": 0,
+            "throttlingPercent": 0,
+            "wmarkRatio": 95,
+            "wmarkScalePermill": 20,
+            "wmarkMinAdj": -25,
+            "priorityEnable": 0,
+            "priority": 0,
+            "oomKillGroup": 0
+          },
+          "resctrlQOS": {
+            "enable": false,
+            "catRangeStartPercent": 0,
+            "catRangeEndPercent": 100,
+            "mbaPercent": 100
+          }
+        },
+        "lsClass": {
+          "cpuQOS": {
+            "enable": false,
+            "groupIdentity": 2
+          },
+          "memoryQOS": {
+            "enable": false,
+            "minLimitPercent": 0,
+            "lowLimitPercent": 0,
+            "throttlingPercent": 0,
+            "wmarkRatio": 95,
+            "wmarkScalePermill": 20,
+            "wmarkMinAdj": -25,
+            "priorityEnable": 0,
+            "priority": 0,
+            "oomKillGroup": 0
+          },
+          "resctrlQOS": {
+            "enable": false,
+            "catRangeStartPercent": 0,
+            "catRangeEndPercent": 100,
+            "mbaPercent": 100
+          }
+        },
+        "beClass": {
+          "cpuQOS": {
+            "enable": false,
+            "groupIdentity": -1
+          },
+          "memoryQOS": {
+            "enable": false,
+            "minLimitPercent": 0,
+            "lowLimitPercent": 0,
+            "throttlingPercent": 0,
+            "wmarkRatio": 95,
+            "wmarkScalePermill": 20,
+            "wmarkMinAdj": 50,
+            "priorityEnable": 0,
+            "priority": 0,
+            "oomKillGroup": 0
+          },
+          "resctrlQOS": {
+            "enable": false,
+            "catRangeStartPercent": 0,
+            "catRangeEndPercent": 30,
+            "mbaPercent": 100
+          }
+        }
+      },
+      "nodeStrategies": [
+        {
+          "nodeSelector": {
+            "matchLabels": {
+              "kubernetes.io/kernel": "alios"
+            }
+          },
+          "beClass": {
+            "memoryQOS": {
+              "wmarkRatio": 90
+            }
+          }
+        }
+      ]
+    }
+  resource-threshold-config: |
+    {
+      "clusterStrategy": {
+        "enable": false,
+        "cpuSuppressThresholdPercent": 65,
+        "cpuSuppressPolicy": "cpuset",
+        "memoryEvictThresholdPercent": 70
+      },
+      "nodeStrategies": [
+        {
+          "nodeSelector": {
+            "matchLabels": {
+              "kubernetes.io/kernel": "alios"
+            }
+          },
+          "cpuEvictBEUsageThresholdPercent": 80
+        }
+      ]
+    }
+kind: ConfigMap
+metadata:
+  annotations:
+    meta.helm.sh/release-name: koordinator
+    meta.helm.sh/release-namespace: default
+  labels:
+    app.kubernetes.io/managed-by: Helm
+  name: slo-controller-config
+  namespace: koordinator-system
+```
 */
