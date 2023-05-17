677d770ae691e64d912edac8f047925d56d2bd93
saintube <saintube@foxmail.com>
2023-05-17 21:08:32 +0800

    chore: move koord-manager scheme under options, add host run mount
    Signed-off-by: saintube <saintube@foxmail.com>

---
+++ cmd/koord-manager/options/scheme.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2022 The Koordinator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package options
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/scheduler-plugins/pkg/apis/scheduling/v1alpha1"
+
+	configv1alpha1 "github.com/koordinator-sh/koordinator/apis/config/v1alpha1"
+	schedulingv1alpha1 "github.com/koordinator-sh/koordinator/apis/scheduling/v1alpha1"
+	slov1alpha1 "github.com/koordinator-sh/koordinator/apis/slo/v1alpha1"
+)
+
+var Scheme = runtime.NewScheme()
+
+func init() {
+	_ = clientgoscheme.AddToScheme(Scheme)
+	_ = configv1alpha1.AddToScheme(clientgoscheme.Scheme)
+	_ = slov1alpha1.AddToScheme(clientgoscheme.Scheme)
+	_ = schedulingv1alpha1.AddToScheme(clientgoscheme.Scheme)
+
+	_ = configv1alpha1.AddToScheme(Scheme)
+	_ = slov1alpha1.AddToScheme(Scheme)
+	_ = schedulingv1alpha1.AddToScheme(Scheme)
+	_ = v1alpha1.AddToScheme(Scheme)
+
+	Scheme.AddUnversionedTypes(metav1.SchemeGroupVersion, &metav1.UpdateOptions{}, &metav1.DeleteOptions{}, &metav1.CreateOptions{})
+	// +kubebuilder:scaffold:scheme
+}
