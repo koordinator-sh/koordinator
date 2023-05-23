742160aa24fbcd0fa3f763a31d35a4fccbeea53e
wangjianyu.wjy <wangjianyu.wjy@alibaba-inc.com>
2023-05-23 10:51:55 +0800

    koord-descheduler: allow annotated pod only pass non-retriable filter
    Signed-off-by: wangjianyu.wjy <wangjianyu.wjy@alibaba-inc.com>

--- pkg/descheduler/controllers/migration/controller.go
+++ pkg/descheduler/controllers/migration/controller.go
@@ -414,18 +414,17 @@ func (r *Reconciler) preparePendingJob(ctx context.Context, job *sev1alpha1.PodM
 			return reconcile.Result{}, err
 		}
 	}
-
+	markPodPrepareMigrating(pod)
 	if !evictionsutil.HaveEvictAnnotation(job) {
-		markPodPrepareMigrating(pod)
 		if aborted, err := r.abortJobIfNonRetryablePodFilterFailed(ctx, pod, job); aborted || err != nil {
 			if err == nil {
 				err = fmt.Errorf("abort job since failed to non-retryable Pod filter")
 			}
 			return reconcile.Result{}, err
 		}
-		if requeue, err := r.requeueJobIfRetryablePodFilterFailed(ctx, pod, job); requeue || err != nil {
-			return reconcile.Result{RequeueAfter: defaultRequeueAfter}, err
-		}
+	}
+	if requeue, err := r.requeueJobIfRetryablePodFilterFailed(ctx, pod, job); requeue || err != nil {
+		return reconcile.Result{RequeueAfter: defaultRequeueAfter}, err
 	}
 
 	job.Status.Phase = sev1alpha1.PodMigrationJobRunning
