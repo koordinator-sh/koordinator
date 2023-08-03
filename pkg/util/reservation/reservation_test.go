fa24d7e502d59e2e74d2ba8daebc21867716c6ae
Joseph <joseph.t.lee@outlook.com>
2023-08-03 11:39:04 +0800

    koord-scheduler: set default priority in reserve pod
    Signed-off-by: Joseph <joseph.t.lee@outlook.com>

--- pkg/util/reservation/reservation_test.go
+++ pkg/util/reservation/reservation_test.go
@@ -56,6 +56,7 @@ func TestNewReservePod(t *testing.T) {
 		}
 		reservePod := NewReservePod(r)
 		assert.NotNil(t, reservePod)
+		assert.NotNil(t, reservePod.Spec.Priority)
 		assert.True(t, IsReservePod(reservePod))
 	})
 }
