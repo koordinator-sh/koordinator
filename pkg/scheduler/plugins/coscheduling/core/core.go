1feadb50bd24e2392ef757853444600b916e319e
bogo <70442600+bogo-y@users.noreply.github.com>
2023-12-18 14:58:49 +0800

    delete redundant argument
    Signed-off-by: bogo <70442600+bogo-y@users.noreply.github.com>

--- pkg/scheduler/plugins/coscheduling/core/core.go
+++ pkg/scheduler/plugins/coscheduling/core/core.go
@@ -272,7 +272,7 @@ func (pgMgr *PodGroupManager) PreFilter(ctx context.Context, pod *corev1.Pod) er
 // ii. If non-strict mode, we will do nothing.
 func (pgMgr *PodGroupManager) PostFilter(ctx context.Context, pod *corev1.Pod, handle framework.Handle, pluginName string, filteredNodeStatusMap framework.NodeToStatusMap) (*framework.PostFilterResult, *framework.Status) {
 	if !util.IsPodNeedGang(pod) {
-		return &framework.PostFilterResult{}, framework.NewStatus(framework.Unschedulable, "")
+		return &framework.PostFilterResult{}, framework.NewStatus(framework.Unschedulable)
 	}
 	gang := pgMgr.GetGangByPod(pod)
 	if gang == nil {
