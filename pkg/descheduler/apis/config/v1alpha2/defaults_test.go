63b24f15081b06cb1f37916e64685fc1ff114cdb
bogo <bogo.ysh@gmail.com>
2024-03-18 10:57:39 +0800

    unit test
    Signed-off-by: bogo <bogo.ysh@gmail.com>

--- pkg/descheduler/apis/config/v1alpha2/defaults_test.go
+++ pkg/descheduler/apis/config/v1alpha2/defaults_test.go
@@ -41,6 +41,23 @@ func TestSetDefaults_LowNodeLoadArgs(t *testing.T) {
 				NodeFit:                     pointer.Bool(false),
 				NodeMetricExpirationSeconds: pointer.Int64(defaultNodeMetricExpirationSeconds),
 				AnomalyCondition:            defaultLoadAnomalyCondition,
+				DetectorCacheTimeout:        &metav1.Duration{Duration: 5 * time.Minute},
+				ResourceWeights: map[corev1.ResourceName]int64{
+					corev1.ResourceCPU:    1,
+					corev1.ResourceMemory: 1,
+				},
+			},
+		},
+		{
+			name: "set detectorCacheTimeout",
+			args: &LowNodeLoadArgs{
+				DetectorCacheTimeout: &metav1.Duration{Duration: 10 * time.Minute},
+			},
+			expected: &LowNodeLoadArgs{
+				NodeFit:                     pointer.Bool(true),
+				NodeMetricExpirationSeconds: pointer.Int64(defaultNodeMetricExpirationSeconds),
+				AnomalyCondition:            defaultLoadAnomalyCondition,
+				DetectorCacheTimeout:        &metav1.Duration{Duration: 10 * time.Minute},
 				ResourceWeights: map[corev1.ResourceName]int64{
 					corev1.ResourceCPU:    1,
 					corev1.ResourceMemory: 1,
@@ -64,6 +81,7 @@ func TestSetDefaults_LowNodeLoadArgs(t *testing.T) {
 					ConsecutiveAbnormalities: defaultLoadAnomalyCondition.ConsecutiveAbnormalities,
 					ConsecutiveNormalities:   3,
 				},
+				DetectorCacheTimeout: &metav1.Duration{Duration: 5 * time.Minute},
 				ResourceWeights: map[corev1.ResourceName]int64{
 					corev1.ResourceCPU:    1,
 					corev1.ResourceMemory: 1,
@@ -87,6 +105,7 @@ func TestSetDefaults_LowNodeLoadArgs(t *testing.T) {
 				NodeFit:                     pointer.Bool(true),
 				NodeMetricExpirationSeconds: pointer.Int64(defaultNodeMetricExpirationSeconds),
 				AnomalyCondition:            defaultLoadAnomalyCondition,
+				DetectorCacheTimeout:        &metav1.Duration{Duration: 5 * time.Minute},
 				LowThresholds: ResourceThresholds{
 					corev1.ResourceCPU:    30,
 					corev1.ResourceMemory: 30,
