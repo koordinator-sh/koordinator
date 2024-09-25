6c0835ecbbb319af06b13c6846830396603358a1
j4ckstraw <j4ckstraw@foxmail.com>
2024-09-25 19:57:19 +0800

    feat: support change pod CPUQOS by annotations
    Signed-off-by: j4ckstraw <j4ckstraw@foxmail.com>

--- pkg/koordlet/runtimehooks/hooks/groupidentity/interceptor.go
+++ pkg/koordlet/runtimehooks/hooks/groupidentity/interceptor.go
@@ -21,6 +21,7 @@ import (
 	"k8s.io/utils/pointer"
 
 	ext "github.com/koordinator-sh/koordinator/apis/extension"
+	slov1alpha1 "github.com/koordinator-sh/koordinator/apis/slo/v1alpha1"
 	"github.com/koordinator-sh/koordinator/pkg/koordlet/runtimehooks/protocol"
 	"github.com/koordinator-sh/koordinator/pkg/koordlet/util"
 )
@@ -30,11 +31,33 @@ func (b *bvtPlugin) SetPodBvtValue(p protocol.HooksProtocol) error {
 	if r == nil {
 		return nil
 	}
+
 	podCtx := p.(*protocol.PodContext)
 	req := podCtx.Request
 	podQOS := ext.GetQoSClassByAttrs(req.Labels, req.Annotations)
 	podKubeQOS := util.GetKubeQoSByCgroupParent(req.CgroupParent)
 	podBvt := r.getPodBvtValue(podQOS, podKubeQOS)
+
+	// pod annotatations take precedence to the default CPUQoS which retrieve from getPodBvtValue
+	cfg, err := slov1alpha1.GetPodCPUQoSConfigByAttr(req.Labels, req.Annotations)
+	if err != nil {
+		return err
+	}
+	// we can change group identity by pod annotations only when QoS=LS
+	// because LS=2 and BE=-1 by default
+	// we only allow LS change to BE
+	// and not allow BE change to LS
+	// and do not change LSR/LSE
+	if cfg != nil && podQOS == ext.QoSLS {
+		if cfg.GroupIdentity != nil {
+			podBvt = *cfg.GroupIdentity
+		}
+
+		// if disabled, set bvt to zero
+		if cfg.Enable == nil || (cfg.Enable != nil && !(*cfg.Enable)) {
+			podBvt = 0
+		}
+	}
 	podCtx.Response.Resources.CPUBvt = pointer.Int64(podBvt)
 	return nil
 }
