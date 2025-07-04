98123557495e9faf2d97aaabfc7dae286a9b9bef
googs1025 <googs1025@gmail.com>
2025-07-04 16:21:24 +0800

    feat(metric): sort podsMetricInfo and hostAppMetricInfo by name for consistent output
    Signed-off-by: googs1025 <googs1025@gmail.com>

--- pkg/koordlet/statesinformer/impl/states_nodemetric.go
+++ pkg/koordlet/statesinformer/impl/states_nodemetric.go
@@ -21,6 +21,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"reflect"
+	"sort"
 	"sync"
 	"time"
 
@@ -387,6 +388,9 @@ func (r *nodeMetricInformer) collectMetric() (*slov1alpha1.NodeMetricInfo, []*sl
 		}
 		podsMetricInfo = append(podsMetricInfo, podMetric)
 	}
+	sort.Slice(podsMetricInfo, func(i, j int) bool {
+		return podsMetricInfo[i].Name < podsMetricInfo[j].Name
+	})
 	for _, hostApp := range nodeSLO.Spec.HostApplications {
 		appMetric, err := r.collectHostAppMetric(&hostApp, queryParam)
 		if err != nil {
@@ -395,6 +399,9 @@ func (r *nodeMetricInformer) collectMetric() (*slov1alpha1.NodeMetricInfo, []*sl
 		}
 		hostAppMetricInfo = append(hostAppMetricInfo, appMetric)
 	}
+	sort.Slice(hostAppMetricInfo, func(i, j int) bool {
+		return hostAppMetricInfo[i].Name < hostAppMetricInfo[j].Name
+	})
 
 	prodReclaimable := &slov1alpha1.ReclaimableMetric{}
 	if p, err := prodPredictor.GetResult(); err != nil {
