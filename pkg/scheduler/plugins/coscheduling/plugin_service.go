0364e8f74e261280ace8af319defa70d9e0471a4
longhui.li <longhui.li@woqutech.com>
2025-09-12 18:15:02 +0800

    Fix: register handle Once when schedule with multi profiles
    Signed-off-by: longhui.li <longhui.li@woqutech.com>
    Co-authored-by: longhui.li <longhui.li@woqutech.com>

--- pkg/scheduler/plugins/coscheduling/plugin_service.go
+++ pkg/scheduler/plugins/coscheduling/plugin_service.go
@@ -12,19 +12,21 @@ import (
 var _ services.APIServiceProvider = &Coscheduling{}
 
 func (cs *Coscheduling) RegisterEndpoints(group *gin.RouterGroup) {
-	group.GET("/gang/:namespace/:name", func(c *gin.Context) {
-		gangNamespace := c.Param("namespace")
-		gangName := c.Param("name")
-		gangId := util.GetId(gangNamespace, gangName)
-		gangSummary, exist := cs.pgMgr.GetGangSummary(gangId)
-		if !exist {
-			services.ResponseErrorMessage(c, http.StatusNotFound, "cannot find gang %s/%s", gangNamespace, gangName)
-			return
-		}
-		c.JSON(http.StatusOK, gangSummary)
-	})
-	group.GET("/gangs", func(c *gin.Context) {
-		allGangSummaries := cs.pgMgr.GetGangSummaries()
-		c.JSON(http.StatusOK, allGangSummaries)
+	cs.handleOnce.Do(func() {
+		group.GET("/gang/:namespace/:name", func(c *gin.Context) {
+			gangNamespace := c.Param("namespace")
+			gangName := c.Param("name")
+			gangId := util.GetId(gangNamespace, gangName)
+			gangSummary, exist := cs.pgMgr.GetGangSummary(gangId)
+			if !exist {
+				services.ResponseErrorMessage(c, http.StatusNotFound, "cannot find gang %s/%s", gangNamespace, gangName)
+				return
+			}
+			c.JSON(http.StatusOK, gangSummary)
+		})
+		group.GET("/gangs", func(c *gin.Context) {
+			allGangSummaries := cs.pgMgr.GetGangSummaries()
+			c.JSON(http.StatusOK, allGangSummaries)
+		})
 	})
 }
