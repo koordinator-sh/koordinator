b5013b6e868500478a59568640655ba277d81064
ZhuZhezz <zzhuzju@163.com>
2025-10-22 16:37:02 +0800

    koordlet: read rdma minor from ibdev and add rdma health status
    Signed-off-by: ZhuZhezz <zzhuzju@163.com>

--- pkg/koordlet/metricsadvisor/devices/rdma/collector_rdma_linux.go
+++ pkg/koordlet/metricsadvisor/devices/rdma/collector_rdma_linux.go
@@ -18,7 +18,12 @@ package rdma
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sort"
 	"strconv"
+	"strings"
 
 	"github.com/Mellanox/rdmamap"
 	"github.com/jaypipes/ghw"
@@ -28,6 +33,15 @@ import (
 	"github.com/koordinator-sh/koordinator/pkg/koordlet/metricsadvisor/devices/helper"
 	"github.com/koordinator-sh/koordinator/pkg/koordlet/util"
 	"github.com/koordinator-sh/koordinator/pkg/koordlet/util/system"
+)
+
+const (
+	SysInfinibandDir = "/sys/class/infiniband"
+	RDMAStateActive  = "ACTIVE"
+)
+
+var (
+	rdmaRegex = regexp.MustCompile(`^mlx\d+_(\d+)$`)
 )
 
 func GetNetDevice() (metriccache.Devices, error) {
@@ -45,21 +59,42 @@ func GetNetDevice() (metriccache.Devices, error) {
 		if !isNetDevice(device.Class.ID) || system.IsSriovVF(device.Address) {
 			continue
 		}
+
+		rdmaResources := rdmamap.GetRdmaDevicesForPcidev(device.Address)
+		if len(rdmaResources) == 0 {
+			klog.Warningf("getNetDevice(): no rdma device for pci device %s", device.Address)
+			continue
+		}
+
+		sort.Slice(rdmaResources, func(i, j int) bool {
+			return rdmaResources[i] < rdmaResources[j]
+		})
+		// pf is loaded first, so the ibdev name is smaller
+		rdmaResource := rdmaResources[0]
+		minor, err := getRDMAMinor(rdmaResource)
+		if err != nil {
+			klog.Errorf("getNetDevice(): get rdma minorID for rdma device %s error, %v", rdmaResource, err)
+			return nil, err
+		}
+
+		deviceHealth := true
+		// check rdma device health
+		if !isRDMADeviceHealthy(rdmaResource) {
+			deviceHealth = false
+		}
 		netDevice := util.RDMADeviceInfo{
 			ID:            device.Address,
 			RDMAResources: rdmamap.GetRdmaDevicesForPcidev(device.Address),
 			VFEnabled:     system.SriovConfigured(device.Address),
 			VFMap:         nil,
-			Minor:         0,
+			Minor:         minor,
 			Labels:        nil,
 			VendorCode:    device.Vendor.ID,
 			DeviceCode:    device.Product.ID,
 			BusID:         device.Address,
+			Health:        deviceHealth,
 		}
-		if len(netDevice.RDMAResources) == 0 {
-			klog.Warningf("getNetDevice(): no rdma device for pci device %s", device.Address)
-			continue
-		}
+
 		nodeID, pcie, _, err := helper.ParsePCIInfo(device.Address)
 		if err != nil {
 			klog.Errorf("getNetDevice(): parse pci device %s error, %v", device.Address, err)
@@ -106,4 +141,41 @@ func isNetDevice(devClassID string) bool {
 // parseDeviceClassID returns device ID parsed from the string as 64bit integer
 func parseDeviceClassID(deviceID string) (int64, error) {
 	return strconv.ParseInt(deviceID, classIDBaseInt, classIDBitSize)
+}
+
+// if ibdev is mlx5_1, return 1
+func getRDMAMinor(rdmaDevice string) (int32, error) {
+	matches := rdmaRegex.FindStringSubmatch(rdmaDevice)
+	if len(matches) != 2 {
+		return -1, fmt.Errorf("rdma device %s format is invalid", rdmaDevice)
+	}
+	minorID, err := strconv.Atoi(matches[1])
+	if err != nil {
+		return -1, fmt.Errorf("rdma device %s minorID parse error: %w", rdmaDevice, err)
+	}
+	return int32(minorID), nil
+}
+
+func isRDMADeviceHealthy(rdmaResource string) bool {
+	// cat /sys/class/infiniband/mlx5_1/ports/1/state
+	portsPath := filepath.Join(SysInfinibandDir, rdmaResource, "ports")
+	ports, err := os.ReadDir(portsPath)
+	if err != nil || len(ports) == 0 {
+		klog.Errorf("isRDMADeviceHealthy(): read rdma device ports dir %s error, %v", portsPath, err)
+		return false
+	}
+	for _, port := range ports {
+		stateFile := filepath.Join(portsPath, port.Name(), "state")
+		// read info from state file
+		rawState, err := os.ReadFile(stateFile)
+		if err != nil {
+			klog.Errorf("isRDMADeviceHealthy(): read rdma device state file %s error, %v", stateFile, err)
+			return false
+		}
+		state := string(rawState)
+		if !strings.Contains(state, RDMAStateActive) {
+			return false
+		}
+	}
+	return true
 }
