11df9fe8f2ee1396901ea1940aaad30017ad065f
Vatsalpatni73 <vatsalpatni73@gmail.com>
2026-06-21 14:12:36 +0530

    Add comments for variables of FragmentationAwareArgs struct
    Signed-off-by: Vatsalpatni73 <vatsalpatni73@gmail.com>

--- pkg/descheduler/apis/config/v1alpha2/types_fragmentationaware.go
+++ pkg/descheduler/apis/config/v1alpha2/types_fragmentationaware.go
@@ -26,24 +26,45 @@ import (
 type FragmentationAwareArgs struct {
 	metav1.TypeMeta `json:",inline"`
 
+	// Paused indicates whether the FragmentationAware plugin is enabled.
+	// Default is false.
 	Paused *bool `json:"paused,omitempty"`
+
+	// DryRun executes the descheduling logic without evicting Pods.
+	// Default is false.
 	DryRun *bool `json:"dryRun,omitempty"`
 
+	// NodeSelector selects the nodes that match the labelSelector.
 	NodeSelector *metav1.LabelSelector `json:"nodeSelector,omitempty"`
 
+	// EvictableNamespaces limits the namespaces of pods that can be evicted.
 	EvictableNamespaces *Namespaces `json:"evictableNamespaces,omitempty"`
 
+	// PodSelectors selects the pods that match the labelSelector.
 	PodSelectors []FragmentationAwarePodSelector `json:"podSelectors,omitempty"`
 
+	// NodeFit enables checking whether a candidate Pod can fit on at least one
+	// other node before eviction, considering scheduling constraints such as node
+	// affinity, taints/tolerations, and available resources.
+	// Default is true.
 	NodeFit *bool `json:"nodeFit,omitempty"`
 
+	// Resources indicates the resource types used to calculate node fragmentation.
+	// The default resources are CPU and memory.
 	Resources []corev1.ResourceName `json:"resources,omitempty"`
 
+	// ImbalanceThreshold specifies the minimum node resource imbalance score required
+	// to consider a node for pod migration.
+	// Default is 0.15.
 	ImbalanceThreshold *float64 `json:"imbalanceThreshold,omitempty"`
 
+	// MinImprovementThreshold specifies the minimum reduction in the node's imbalance
+	// score (std_before - std_after) required to evict a Pod.
+	// Default is 0.02.
 	MinImprovementThreshold *float64 `json:"minImprovementThreshold,omitempty"`
 }
 
 type FragmentationAwarePodSelector struct {
+	// Selector is a label query over pods for migration.
 	Selector *metav1.LabelSelector `json:"selector,omitempty"`
 }
