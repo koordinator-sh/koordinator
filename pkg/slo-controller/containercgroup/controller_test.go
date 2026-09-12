2543b6fba179a33b00a117dd7246196ee0b3f7e6
pingxin403 <pingxin403@163.com>
2026-09-12 09:27:55 +0800

    fix: apply gofmt to containercgroup controller_test.go
    Signed-off-by: pingxin403 <pingxin403@163.com>

--- pkg/slo-controller/containercgroup/controller_test.go
+++ pkg/slo-controller/containercgroup/controller_test.go
@@ -23,8 +23,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
